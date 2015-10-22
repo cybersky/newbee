94a3cb4fa29cc0a8f5baa5691b0215eb3f087ec7
孙建 <daniels.sun@runmit.com>
2015-10-22 16:55:10 +0800

    rename exports.pre to setHeaders

--- middleware/setHeaders.js
+++ middleware/setHeaders.js
@@ -2,7 +2,7 @@
  * Created by Daniels on 10/22/15.
  */
 
-module.exports = exports.pre = (req, res, next) => {
+module.exports = exports.setHeaders = (req, res, next) => {
     res.set("x-powered-by", "NewBee");
     return next();
 };
