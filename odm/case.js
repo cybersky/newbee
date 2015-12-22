7900b8e4579199a1a78e6004d3c51ba015c45e88
赵海龙 <hailong.zhao@runmit.com>
2015-12-22 13:19:54 +0800

    case api

--- odm/case.js
+++ odm/case.js
@@ -35,11 +35,17 @@ exports.createCase = function(userCase){
     if (userCase.price2 && !isNaN(Number(userCase.price2)) && ( Number(userCase.price2) < 0 || Number(userCase.price2) > 100) )
         return callback({rtn: config.errorCode.paramError, message: locale.price2FormatError});
 
+    if (userCase.lon && isNaN(Number(userCase.lon)))
+        return callback({rtn: config.errorCode.paramError, message: locale.price1FormatError});
+
+    if (userCase.lat && isNaN(Number(userCase.lat)))
+        return callback({rtn: config.errorCode.paramError, message: locale.price1FormatError});
+
     if(!userCase.userOpenId){
         return callback({rtn: config.errorCode.paramError, message: locale.wxOpenIdError});
     }
 
-    userCase.status = config.caseStatus.RAW.key;
+    userCase.status = config.caseStatus.raw.key;
     userCase.createdAt = new Date();
     userCase.updatedAt = new Date();
 
@@ -59,12 +65,67 @@ exports.updateCase = function(caseId, caseDoc) {
     var callback = arguments[arguments.length-1];
     if(typeof(callback) != 'function') throw new Error('callback should be function.');
 
+
+
+    if (caseDoc.userCaseType && _.pluck(config.userCaseType, 'name').indexOf(caseDoc.caseType) < 0)
+        return callback({rtn: config.errorCode.paramError, message: locale.unknowCaseType});
+
+    if (caseDoc.userServiceType && _.pluck(config.userServiceType, 'name').indexOf(caseDoc.serviceType) < 0)
+        return callback({rtn: config.errorCode.paramError, message: locale.unkownServiceType});
+
+    if (caseDoc.caseDesc && caseDoc.caseDesc.length < 20)
+        return callback({rtn: config.errorCode.paramError, message: locale.tooShortDesc});
+
+    if (caseDoc.caseTarget && caseDoc.caseTarget.length < 20)
+        return callback({rtn: config.errorCode.paramError, message: locale.tooShortTarget});
+
+    if (caseDoc.price1 && caseDoc.price2)
+        return callback({rtn: config.errorCode.paramError, message: locale.eitherPrice});
+
+    if (caseDoc.price1 && isNaN(Number(caseDoc.price1)))
+        return callback({rtn: config.errorCode.paramError, message: locale.price1FormatError});
+
+    if (caseDoc.price2 && !isNaN(Number(caseDoc.price2)) && ( Number(caseDoc.price2) < 0 || Number(caseDoc.price2) > 100) )
+        return callback({rtn: config.errorCode.paramError, message: locale.price2FormatError});
+
+    if (caseDoc.lon && isNaN(Number(caseDoc.lon)))
+        return callback({rtn: config.errorCode.paramError, message: locale.lonFormatError});
+
+    if (caseDoc.lat && isNaN(Number(caseDoc.lat)))
+        return callback({rtn: config.errorCode.paramError, message: locale.latFormatError});
+
+
     var caseCollection = mongo.case();
 
     caseCollection.update({_id:new ObjectID(caseId)}, {$set:caseDoc}, function(err, result){
         callback(err, result);
     });
 };
+
+
+exports.getCaseByServiceType = function(type){
+
+};
+
+
+exports.getCaseByCaseType = function(type){
+
+};
+
+
+exports.getCaseByStatus = function(state, option){
+    var callback = arguments[arguments.length-1];
+    if(typeof(callback) != 'function') throw new Error('callback should be function.');
+
+    var states = _.pluck(_.values(config.caseStatus), 'key');
+    if( states.indexOf(state) < 0 ) return callback({rtn:config.errorCode.paramError, message:'invalid case state'});
+
+    option = option || {};
+    option.sort = option.sort || {createdAt:-1, updatedAt:-1};
+    exports.getCase({status:state}, option, callback);
+};
+
+
 
 
 exports.getCase = function(query, option){
